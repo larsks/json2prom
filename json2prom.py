1a897e360ecde2bdec1aefbd1d621caf1ebbe987
Lars Kellogg-Stedman <lars@oddbit.com>
2022-01-25 08:00:33 -0500

    Rewrite code to use prometheus_client
    Use prometheus_client to create metrics and manage the webserver. This
    simplifies the code and requires us to refactor the config file format.

--- json2prom.py
+++ json2prom.py
@@ -5,21 +5,34 @@ import json
 import logging
 import os
 
-from aiohttp import web
-from jsonpath_ng import parse
+import jsonpath_ng as jsonpath
+import prometheus_client as prom
 
 LOG = logging.getLogger(__name__)
 
 
 class Transformer:
-    def __init__(self, nameprefix, labels, sources, interval=300):
-        self.nameprefix = nameprefix
-        self.labels = labels
-        self.sources = sources
+    def __init__(self, config, interval=300):
+        self.config = config
         self.interval = interval
 
-        self.metrics = []
+        self.metrics = {}
         self.lock = asyncio.Lock()
+
+        self.create_metrics()
+
+    def create_metrics(self):
+        defaults = self.config.get("defaults", {})
+        labels = ["source"] + self.config.get("labels", [])
+        for mspec in self.config["metrics"]:
+            metric_name = f"{self.config.get('nameprefix', '')}{mspec['name']}"
+            mtype = mspec.get("type", defaults.get("type", "gauge")).title()
+            LOG.info("creating %s metric %s", mtype, metric_name)
+
+            mclass = getattr(prom, mtype)
+            self.metrics[metric_name] = mclass(
+                metric_name, mspec.get("description", ""), labels
+            )
 
     async def loop(self):
         while True:
@@ -28,105 +41,89 @@ class Transformer:
 
     async def update_metrics(self):
         LOG.info("updating metrics")
+
         tasks = []
-        for src in self.sources:
-            for url in src["urls"]:
-                tasks.append(asyncio.create_task(self.fetch_url(src, url)))
+        for source in self.config["sources"]:
+            for url in source["urls"]:
+                tasks.append(asyncio.create_task(self.fetch_url(source, url)))
 
-        res = await asyncio.gather(*tasks)
+        results = await asyncio.gather(*tasks)
+        for success, src, urlspec, data in results:
+            if not success:
+                LOG.info("skipping %s: failed", src["name"])
+                continue
 
-        async with self.lock:
-            self.metrics = []
-            for success, src, urlspec, data in res:
-                if not success:
+            labels = {"source": src["name"]}
+            labels.update(urlspec.get("labels", {}))
+
+            for mspec in src["metrics"]:
+                metric_name = f"{self.config.get('nameprefix', '')}{mspec['name']}"
+                expr = jsonpath.parse(mspec["path"])
+                match = expr.find(data)
+                if len(match) != 1:
+                    LOG.error(
+                        "found %d values for source %s metric %s",
+                        len(match),
+                        src["name"],
+                        metric_name,
+                    )
                     continue
 
-                for path, name in src["expose"].items():
-                    expr = parse(path)
-                    match = expr.find(data)
-                    if len(match) != 1:
-                        continue
-
-                    val = match[0].value
-
-                    if self.nameprefix:
-                        metric_name = f"{self.nameprefix}{name}"
-                    else:
-                        metric_name = name
-
-                    metric = {
-                        "name": metric_name,
-                        "labels": {
-                            "source": src["name"],
-                        },
-                        "value": val,
-                    }
-                    if self.labels:
-                        metric["labels"].update(self.labels)
-                    if "labels" in urlspec:
-                        metric["labels"].update(urlspec["labels"])
-                    self.metrics.append(metric)
+                val = match[0].value
+                self.metrics[metric_name].labels(**labels).set(val)
+                LOG.debug("source %s metric %s = %s", src["name"], metric_name, val)
 
     async def fetch_url(self, src, urlspec):
         url = urlspec["url"].format(**os.environ)
-        LOG.info("fetch %s", url)
+        LOG.info("fetch source %s url %s", src["name"], url)
         async with httpx.AsyncClient() as client:
             try:
                 res = await client.get(url)
                 res.raise_for_status()
                 success = True
-            except Exception as err:
+            except httpx.HTTPStatusError as err:
                 LOG.error("failed to fetch %s: %s", url, err)
                 success = False
 
             return success, src, urlspec, res.json()
 
-    async def get_metrics(self):
-        async with self.lock:
-            return self.metrics
 
-
-def start_background_tasks(T):
-    async def _start_wrapper(app):
-        app["update_metrics"] = asyncio.create_task(T.loop())
-
-    return _start_wrapper
-
-
-def create_app(config_file="config.json"):
-    with open(config_file) as fd:
-        config = json.load(fd)
-
-    T = Transformer(config.get("nameprefix"), config.get("labels"), config["sources"])
-
-    routes = web.RouteTableDef()
-    app = web.Application()
-
-    app.on_startup.append(start_background_tasks(T))
-
-    @routes.get("/metrics")
-    async def metrics(request):
-        res = []
-        for metric in T.metrics:
-            labels = ", ".join(f'{k}="{v}"' for k, v in metric["labels"].items())
-            res.append(f"{metric['name']}{{{labels}}} {metric['value']}")
-
-        return web.Response(text="\n".join(res))
-
-    app.add_routes(routes)
-    return app
-
-
-def main():
+def parse_args():
     p = argparse.ArgumentParser()
     p.add_argument(
         "--config-file",
         "-f",
         default=os.environ.get("JSON2PROM_CONFIG_FILE", "config.json"),
     )
-    args = p.parse_args()
-    app = create_app(config_file=args.config_file)
-    web.run_app(app)
+    p.add_argument("--interval", "-i", type=int, default=300)
+    p.add_argument("--port", "-p", type=int, default=8080)
+    p.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=int(os.environ.get("J2P_LOG_LEVEL", "0")),
+    )
+
+    return p.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    loglevel = ["WARNING", "INFO", "DEBUG"][min(args.verbose, 2)]
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=loglevel,
+    )
+
+    with open(args.config_file) as fd:
+        config = json.load(fd)
+
+    T = Transformer(config, interval=args.interval)
+
+    prom.start_http_server(args.port)
+    asyncio.run(T.loop())
 
 
 if __name__ == "__main__":
